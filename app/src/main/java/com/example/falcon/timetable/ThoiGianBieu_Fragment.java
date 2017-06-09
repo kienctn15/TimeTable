ea6ca198cd8aec921ed1e3e3be7164e98ea5c7d3
kienctn15 <kienctn15@gmail.com>
2017-06-09 15:58:32 +0700

    thoigianbieu

--- app/src/main/java/com/example/falcon/timetable/ThoiGianBieu_Fragment.java
+++ app/src/main/java/com/example/falcon/timetable/ThoiGianBieu_Fragment.java
@@ -1,5 +1,6 @@
 package com.example.falcon.timetable;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
@@ -10,11 +11,18 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.falcon.timetable.DBHandler.DBHandler;
+import com.example.falcon.timetable.DanhSachCongViec.CongViec;
 import com.example.falcon.timetable.DanhSachCongViec.ThemCongViecFragment;
 import com.github.sundeepk.compactcalendarview.CompactCalendarView;
 import com.github.sundeepk.compactcalendarview.domain.Event;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
@@ -28,13 +36,15 @@ public class ThoiGianBieu_Fragment extends Fragment {
     View myView;
     CompactCalendarView compactCalendar;
     TextView tv_month, tv_next, tv_previous;
+    DBHandler db;
 
     private SimpleDateFormat dateFormatMonth = new SimpleDateFormat("MMMM - yyyy", Locale.getDefault());
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.fragment_thoigianbieu, container, false);
-
+        db = new DBHandler(getActivity());
         initFindViewById();
         initEvent();
         compactCalendar = (CompactCalendarView) myView.findViewById(R.id.compactcalendar_view);
@@ -42,6 +52,9 @@ public class ThoiGianBieu_Fragment extends Fragment {
         compactCalendar.setUseThreeLetterAbbreviation(true);
         String abc = dateFormatMonth.format(compactCalendar.getFirstDayOfCurrentMonth()); // hiển thị tháng - năm bao nhiêu
         tv_month.setText(abc);
+
+        addToCalendar();
+
 
         FloatingActionButton fab = (FloatingActionButton) myView.findViewById(R.id.fab);
 
@@ -88,6 +101,7 @@ public class ThoiGianBieu_Fragment extends Fragment {
         tv_previous.setText("<");
         tv_next.setText(">");
     }
+
     private void initEvent() {
         tv_previous.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -106,5 +120,47 @@ public class ThoiGianBieu_Fragment extends Fragment {
             }
         });
 
+    }
+
+    private void addToCalendar() {
+        List<CongViec> list = new ArrayList<>();
+        list = db.get_all_congviec();
+        if (!list.isEmpty()) {
+            for (int i = 0; i < list.size(); i++) {
+                CongViec congViec = list.get(i);
+                int id = congViec.getId();
+                String title = congViec.getTitle();
+                String address = congViec.getAddress();
+                String date = congViec.getDate();
+                String start = congViec.getTime_start();
+                String end = congViec.getTime_end();
+                String note = congViec.getNote();
+
+                System.out.println(id + "\t" + title + "\t" + address + "\t" + date + "\t" + start + "\t" + end + "\t" + note);
+
+                JSONObject obj = new JSONObject();
+                try {
+                    obj.put("id", id);
+                    obj.put("title", title);
+                    obj.put("address", address);
+                    obj.put("date", date);
+                    obj.put("start", start);
+                    obj.put("end", end);
+                    obj.put("note", note);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                long epoch = 0;
+                try {
+                    epoch = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").parse(date + " " + start+   ":00").getTime() / 1000;
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                String epoch1 = String.valueOf(epoch) + "000";
+                Event event = new Event(Color.RED, Long.parseLong(epoch1), obj);
+                compactCalendar.addEvent(event);
+            }
+        }
     }
 }
