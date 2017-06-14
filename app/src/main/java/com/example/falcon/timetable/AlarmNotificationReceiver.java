5ba97b7c0430fe7fc8e3631d7fa411f7a7c3bac1
kienctn15 <kienctn15@gmail.com>
2017-06-14 15:04:29 +0700

    ahihi

--- app/src/main/java/com/example/falcon/timetable/AlarmNotificationReceiver.java
+++ app/src/main/java/com/example/falcon/timetable/AlarmNotificationReceiver.java
@@ -2,10 +2,14 @@ package com.example.falcon.timetable;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.NotificationCompat;
+import android.widget.RemoteViews;
+
+import static android.telephony.PhoneNumberUtils.PAUSE;
 
 /**
  * Created by Optimus on 06/13/17.
@@ -24,5 +28,6 @@ public class AlarmNotificationReceiver extends BroadcastReceiver {
                 .setContentText("Bạn có việc cần làm ngay bây giờ đó!");
         NotificationManager notificationManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(1, builder.build());
+
     }
 }
